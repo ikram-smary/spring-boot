5e60a15cae7a7f882373c121023986e5d96fe133
Ikram <smariikram@gmail.com>
2023-03-13 16:21:28 +0100

    initializing spring boot project

---
+++ src/test/java/com/karrou/SpringbootexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karrou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
